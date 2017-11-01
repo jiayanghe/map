2838e4a5eb59186ba882187cd3005a88ec47709e
Young <Young@JIAYANGdeMBP.lan>
2017-11-01 15:16:47 +1000

    hide content when menu is shrinked

--- js/app.js
+++ js/app.js
@@ -8,41 +8,44 @@ let places = [
 
 let markers = [];
 
-
+function initMap() {
+	
+}
 
 let Place = function(data) {
 	this.name = ko.observable(data.name);
 	this.location = ko.observable(data.location);
 }
 
-function ViewModel() {
-	let map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat:-27.468657, lng:153.023913},
-		zoom: 13
-         });
+let ViewModel = function() {
+	function initMap() {
+		let map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat:-27.468657, lng:153.023913},
+			zoom: 13
+	         });
 
-	places.forEach(function(place) { 
-		place.marker = new google.maps.Marker({
-			position: place.location,
-			map:map,
-			title: place.name,
-			animation: google.maps.Animation.DROP
+		places.forEach(function(place) { 
+			place.marker = new google.maps.Marker({
+				position: place.location,
+				map:map,
+				title: place.name,
+				animation: google.maps.Animation.DROP
+			});
+
+			place.info = new google.maps.InfoWindow({
+				content: place.name
+			});
+
+			place.marker.addListener('click', function() {
+	    		place.info.open(map, place.marker);
+	    		place.marker.setAnimation(google.maps.Animation.BOUNCE);
+	    		setTimeout(function() {
+	    			place.marker.setAnimation(null);
+	    		}, 2800);
+	  		});
+
 		});
-
-		place.info = new google.maps.InfoWindow({
-			content: place.name
-		});
-
-		place.marker.addListener('click', function() {
-    		place.info.open(map, place.marker);
-    		place.marker.setAnimation(google.maps.Animation.BOUNCE);
-    		setTimeout(function() {
-    			place.marker.setAnimation(null);
-    		}, 2800);
-  		});
-
-	});
-
+	};
 	let self = this;
 	this.list = ko.observableArray([]);
 	places.forEach(function(placeItem) {
@@ -54,7 +57,8 @@ function ViewModel() {
 $(document).ready(function(){
 	//show the list when user click the burger icon.
 	$('#icon').click(function() {
-		$('#menu').toggleClass('hide');
+		$('#menu').toggleClass('hideList');
+		$('#search,#list').toggleClass('hide');
 		let map = document.getElementById('map');
 		//shrink the map when the list shows.
 		if (map.classList.contains('movemap')) {
